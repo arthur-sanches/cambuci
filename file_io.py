b3ad07465ea00fcf057cafdb405489a74a7bbee2
arthur-sanches <arthur.sanches96@gmail.com>
2020-08-05 23:05:39 -0300

    Adds encrypt and decrypt functions

--- file_io.py
+++ file_io.py
@@ -1,51 +1,54 @@
 import json
+from cryptography.fernet import Fernet
+
+from crypto_module import get_encryption_key
 
 
 def create(user):
-    name = user.login['user_login']
     try:
-        with open(f'{name}.txt', mode='w', encoding='utf-8') as file:
-            __write(user)
+        __write(user)
     except (IOError):
         print("Unable to create file.")
 
 
 def __write(user):
-    name = user.login['user_login']
     try:
-        with open(f'{name}.txt', mode='w', encoding='utf-8') as file:
+        with open(f'{name(user)}.txt', mode='wb') as file:
             user_info = [user.login, user.password, user.service_accounts]
-            json.dump(user_info, file, ensure_ascii=False)
-            
+            encrypted_data = encrypt(user, user_info)
+            file.write(encrypted_data)
+
     except (IOError):
         print("Unable to write to file.")
 
 
 def read(user):
-    name = user.login['user_login']
-
     try:
-        with open(f'{name}.txt', mode='r', encoding='utf-8') as file:
-            user_info = json.load(file)
+        with open(f'{name(user)}.txt', mode='rb') as file:
+            data = file.read()
+            user_info = decrypt(user, data)
             user.service_accounts = user_info[2]
             user_info
     except (IOError):
         create(user)
 
 
-def return_user(user):
-    name = user.login['user_login']
-
-    try:
-        with open(f'{name}.txt', mode='r', encoding='utf-8') as file:
-            user_info = json.load(file)
-            return user_info
-    except (IOError):
-        pass
+def name(user):
+    return user.login['user_login']
 
 
-def save(user):
-    pass
+def encrypt(user, data):
+    key = get_encryption_key(user)
+    encoded_data = json.dumps(data).encode()
+    f = Fernet(key)
+    encrypted_data = f.encrypt(encoded_data)
+    return encrypted_data
 
-def user_exist(user):
-    pass
+
+def decrypt(user, data):
+    key = get_encryption_key(user)
+    f = Fernet(key)
+    decrypted_data = f.decrypt(data)
+    decoded_data = decrypted_data.decode()
+    json_formatted_data = json.loads(decoded_data)
+    return json_formatted_data
